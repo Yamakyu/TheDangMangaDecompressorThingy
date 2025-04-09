060bfe79026d3d3437800f249206299a266f109d
Yamakyu <ykyupro@yahoo.com>
2025-04-09 21:43:50 +0200

    Sélection de folder OK

---
+++ AutomateMangaWithKCC/AutomateMangaWithKCC/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutomateMangaWithKCC
+{
+    class Program
+    {
+        //[System.AttributeUsage(System.AttributeTargets.Method)]
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ca va jamais marcher du premier coup mdr");
+            string testFolder = PickWorkingFolder();
+            Console.WriteLine(testFolder);
+            Console.ReadLine();
+
+        }
+        private static string PickWorkingFolder()
+        {
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            // Set validate names and check file exists to false otherwise windows will
+            // not let you select "Folder Selection."
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            // Always default to Folder Selection.
+            folderBrowser.FileName = "Folder Selection.";
+            string folderPath = "didn't work";
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = Path.GetDirectoryName(folderBrowser.FileName);
+                // ...
+            }
+            return folderPath;
+            // https://stackoverflow.com/a/50263779
+        }
+    }
+}
